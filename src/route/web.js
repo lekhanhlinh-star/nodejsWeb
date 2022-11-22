49348e9dcd253af2a09f214830a6d429220a8c06
lekhanhlinh-star <82929088+lekhanhlinh-star@users.noreply.github.com>
2022-11-22 20:57:00 +0700

    version 2

--- src/route/web.js
+++ src/route/web.js
@@ -3,12 +3,31 @@ import homeController from "../controller/homeController"
 
 let router=express.Router();
 
+function checkAuth(req, res, next) {
+    if (!req.session.user_id) {
+      res.send('You are not authorized to view this page');
+    } else {
+      next();
+    }
+  }
 const initWebRoute=(app)=>{
-    router.get('/',homeController.getHomepage);
-    router.get('/about',(rep,res)=>{
-        res.send("I'm Linh")
+    router.get('/home',homeController.getHomepage);
+    router.get('/login',(rep,res)=>{
+        res.render("./web/login.ejs",{message:''})
+        
     })
+    router.get('/', function(request, response) {
+      // Render login template
+      response.render("./web/login.ejs",{message:''})
+    });
+    // router.post("/loginCheck", homeController.CheckLogin);
+    router.post('/auth', homeController.CheckLogin);
+    
+    router.post("register",homeController.RegisterUser);
+
+
     return app.use("/",router)
+
 }
 
 export default initWebRoute;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 import  express  from 'express'
+import { join } from 'path'
 import ConfigViewEngine from './configs/ViewEngine'
 import initWebRoute from './route/web'
+import session from 'express-session'
 // import con from './configs/ConnectDB';
 require('dotenv').config()
 
 const path = require('path')
 const app = express()
 const port = process.env.PORT || 8080
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
 ConfigViewEngine(app);
 initWebRoute(app);
